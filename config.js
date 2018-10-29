18f9149046a2ed0331844a05c5bc67e4d8854ea7
Dominik Wilusz <wilusz1@o2.pl>
2018-10-29 20:48:53 +0100

    Finish task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports= {
+    'GOOGLE_CLIENT_ID': '27338304505-rql31ie8338sjheg4ul2co7sdmmilc8t.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'cILaauLsNiGF7WN6nNeJa0wh',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
